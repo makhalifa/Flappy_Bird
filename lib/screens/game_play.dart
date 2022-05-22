c361468dee673b3ce3a349919a22e243c394394b
Abdelrahmanmohamed6 <abdoo.elbasha22@gmail.com>
2022-05-22 23:28:40 +0200

    Update game_play.dart

--- lib/screens/game_play.dart
+++ lib/screens/game_play.dart
@@ -27,12 +27,14 @@ class _GamePlayState extends State<GamePlay> {
       // a real physical jump is the same as an upside down parabola
       // so this is a simple quadratic equation
       height = gravity * time * time + velocity * time;
+
       setState(() {
         birdY = initialPos - height;
       });
 
-      if (birdY < -1.7 || birdY > 1.7) {
+      if (birdIsDead()) {
         timer.cancel();
+        _showDialog();
       }
 
       // keep the time going
@@ -40,11 +42,58 @@ class _GamePlayState extends State<GamePlay> {
     });
   }
 
+  void resetGame() {
+    Navigator.pop(context);
+    setState(() {
+      birdY = 0;
+      gameHasStarted = false;
+      time = 0;
+      initialPos = birdY;
+    });
+  }
+  void _showDialog() {
+    showDialog(
+        context:context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            backgroundColor: Colors.brown,
+            title: Center(
+              child: Text(
+                "G A M E  O V E R",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            actions: [
+              GestureDetector(
+                onTap: resetGame,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(5),
+                  child: Container(
+                    padding: EdgeInsets.all(7),
+                    color: Colors.white,
+                    child: Text(
+                      'Play Again',
+                      style: TextStyle(color: Colors.brown),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          );
+        });
+  }
   void jump() {
     setState(() {
       time = 0;
       initialPos = birdY;
     });
+  }
+  bool birdIsDead() {
+    if (birdY < -1.7 || birdY > 1.7) {
+      return true;
+    }
+    return false;
   }
 
   @override
@@ -60,7 +109,18 @@ class _GamePlayState extends State<GamePlay> {
                 color: Colors.blue,
                 child: Center(
                   child: Stack(
-                    children: [MyBird(birdY)],
+                    children: [
+                      MyBird(
+                        birdY,
+                      ),
+                      Container(
+                        alignment: Alignment(0, -0.5),
+                        child: Text(
+                          gameHasStarted ? '' : 'T A P  T O  PLAY ',
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                      )
+                    ],
                   ),
                 ),
               ),
